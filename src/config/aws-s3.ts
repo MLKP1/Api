186cc0ea2a320c95a625fe975ccf6d1b3f0bce5b
lucaslinyker <lucaslinyker@outlook.com>
2025-09-28 15:49:44 -0300

    feat: add config of aws-sdk and env

---
+++ src/config/aws-s3.ts
@@ -0,0 +1,31 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { env } from '@/env'
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  },
+  region: env.AWS_REGION,
+})
+
+interface saveImageToS3Props {
+  fileName: string
+  arrayBuffer: ArrayBuffer
+  imgType: string
+}
+
+export async function saveImageToS3({
+  fileName,
+  arrayBuffer,
+  imgType,
+}: saveImageToS3Props) {
+  const params = new PutObjectCommand({
+    Bucket: env.AWS_BUCKET_NAME,
+    Key: fileName,
+    Body: Buffer.from(arrayBuffer),
+    ContentType: imgType,
+  })
+
+  await s3.send(params)
+}
--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,11 @@ const envSchema = z.object({
   DB_URL: z.string().url().min(1),
   JWT_SECRET_KEY: z.string().min(1),
   RESEND_API_KEY: z.string().min(1),
+  AWS_ACCESS_KEY_ID: z.string().min(1),
+  AWS_SECRET_ACCESS_KEY: z.string().min(1),
+  AWS_REGION: z.string().min(1),
+  AWS_BUCKET_NAME: z.string().min(1),
+  AWS_ENDPOINT: z.string().url().min(1),
 })
 
 export const env = envSchema.parse(process.env)
