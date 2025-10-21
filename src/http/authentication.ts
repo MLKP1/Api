a3b874f51fab62020e2226dc9d27bffb3c44f78d
lucaslinyker <lucaslinyker@outlook.com>
2025-10-21 17:32:56 -0300

    remove the code from catch the bug

--- src/http/authentication.ts
+++ src/http/authentication.ts
@@ -36,24 +36,9 @@ export const authentication = new Elysia()
   .derive(({ jwt, cookie, setCookie, removeCookie }) => {
     return {
       getCurrentUser: async () => {
-        console.group()
-        if (cookie.auth) {
-          console.info(`Cookie found: ${JSON.stringify(cookie.auth)}`)
-        } else {
-          console.error('No cookie found')
-        }
-
         const payload = await jwt.verify(cookie.auth)
 
-        if (payload) {
-          console.info(`Payload found: ${JSON.stringify(payload)}\n\n`)
-        } else {
-          console.error('No payload found')
-        }
-        console.groupEnd()
-
         if (!payload) {
-          // console.log(`${JSON.stringify(cookie)} --- ${JSON.stringify(payload)}`)
           throw new UnauthorizedError()
         }
 
